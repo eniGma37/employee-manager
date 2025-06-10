2243827e85918a90ce6e52eb5fe5f031c1d99bd9
Dani <daniel_martin_giner@hotmail.com>
2025-06-10 21:22:57 +0200

    feature/added getters and setters

---
+++ Manager/src/Manager/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getters
+    public String getDepartment() {
+        return department;
+    }
+
+    // Setters
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    // Getters
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setters
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return name + " (" + department + ") - $" + salary;
+    }
+}
+
